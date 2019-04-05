849aaf81497ed1539bcc642ed32dc021d397e232
Keegan H <keeganslane@gmail.com>
2019-04-04 23:10:09 -0700

    updating urls

--- pages/urls.py
+++ pages/urls.py
@@ -2,11 +2,12 @@
 
 from django.urls import path
 
-from .views import HomePageView, AboutPageView, TestView, ProjectsView
+from .views import HomePageView, AboutPageView, TestView, ProjectsView, WhateverPage
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('test/', TestView.as_view(), name='test'),
     path('projects/', ProjectsView.as_view(), name='projects'),
     path('about/', AboutPageView.as_view(), name='about'),
+    path('whatever/', WhateverPage.as_view(), name='whatever'),
 ]
